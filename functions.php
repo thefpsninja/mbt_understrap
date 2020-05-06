5a78d0d78c9a491ae88b02707262858632301be3
Sylvester <sylvester.lindfors@gmail.com>
2020-05-06 15:39:22 +0200

    Added custom Post types and Taxonomies

--- functions.php
+++ functions.php
@@ -17,6 +17,8 @@ $understrap_includes = array(
 	'/pagination.php',                      // Custom pagination for this theme.
 	'/hooks.php',                           // Custom hooks.
 	'/extras.php',                          // Custom functions that act independently of the theme templates.
+	'/custom-post-types.php',				// Custom Post types.
+	'/custom-taxonomies.php',				// Custom Taxonomies.
 	'/customizer.php',                      // Customizer additions.
 	'/custom-comments.php',                 // Custom Comments file.
 	'/jetpack.php',                         // Load Jetpack compatibility file.
