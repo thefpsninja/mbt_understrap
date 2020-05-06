243f85cff7b4fbddea008f641fd57b8d5aedf92a
Sylvester <sylvester.lindfors@gmail.com>
2020-05-06 16:20:06 +0200

    Added custom frontpage php

---
+++ front-page.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+
+// Adds Hero widget
+get_template_part('global-templates/hero');
+
+?>
+
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+			<main class="site-main" id="main">
+
+				<?php
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'loop-templates/content', 'frontpage' );
+				}
+				?>
+
+			</main><!-- #main -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #page-wrapper -->
+
+<?php
+get_footer();
---
+++ loop-templates/content-frontpage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Partial template for content in page.php
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
